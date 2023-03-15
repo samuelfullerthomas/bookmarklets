787ff4e33a85d432bc63468383291a2ce70b0374
Sam Thomas <sthomas@coveo.com>
2023-03-15 14:56:36 +0000

    feat: make git bookmarklets redundant

--- bookmarklets/selectClosedOrMerged/index.js
+++ bookmarklets/selectClosedOrMerged/index.js
@@ -1,14 +1,16 @@
 (function() {
   const notificationTypesByClassName = [
-    'octicon-git-merge', // merged pull requests
-    'octicon-git-pull-request-closed', // closed pull requests
+    '.octicon-git-merge', // merged pull requests
+    '.octicon-git-pull-request-closed', // closed pull requests
   ];
 
   notificationTypesByClassName.forEach(notificationClassName => {
-    Array.from(document.getElementsByClassName(notificationClassName)).forEach(
+    Array.from(document.querySelectorAll(notificationClassName)).forEach(
       elem => {
         const notificationElem = elem.closest('.notifications-list-item');
-        notificationElem.querySelector('input').click();
+        if (notificationElem) {
+          notificationElem.querySelector('input').click();
+        }
       }
     );
   });
